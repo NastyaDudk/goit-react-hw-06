9a50634efbcd332590dc986719f615b16d2ad7c3
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2024-04-19 16:29:04 +0400

    ok

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -9,7 +9,7 @@ const ContactSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, "Too Short!")
     .max(50, "Too Long!")
-    .required("Required"),
+    .required("User name is required!"),
   number: Yup.string()
     .matches(/^[0-9 -/+]+$/, "Invalid number. You can use '0-9', ' ', '-', '+'")
     .min(3, "Too Short!")
